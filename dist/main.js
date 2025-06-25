c8b9d1f0ad0faf6a76820bae8498f2b4f162ba01
Tatiana77-git <tatiana.dor@free.fr>
2025-06-25 11:43:07 +0200

    first commit

---
+++ dist/main.js
@@ -0,0 +1,3 @@
+"use strict";
+const myName = "World";
+console.log(`Hello, ${myName}`);
---
+++ src/main.ts
@@ -0,0 +1,96 @@
+const myName: string = "World";
+console.log(`Hello, ${myName}`);
+
+
+const data : MediaData = {
+  books: [
+    {
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      year: 1937,
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      year: 1949,
+    },
+  ],
+  movies: [
+    {
+      title: "Inception",
+      director: "Christopher Nolan",
+      year: 2010,
+    },
+    {
+      title: "Parasite",
+      director: "Bong Joon-ho",
+      year: 2019,
+    },
+  ],
+  albums: [
+    {
+      title: "Thriller",
+      artist: "Michael Jackson",
+      year: 1982,
+    },
+    {
+      title: "Back in Black",
+      artist: "AC/DC",
+      year: 1980,
+    },
+  ],
+};
+
+const appName: string = "Mon application";
+const isOnline: boolean = true;
+const maxItem: number = 10;
+ interface Book {
+  title: string;
+  author: string;
+  year: number;
+}
+ interface Movie {
+    title :string;
+    director : string; 
+    year: number;
+ }
+
+ interface Album {
+  title: string;
+  artist: string;
+  year: number;
+}
+
+interface MediaData {
+  books: Book[];
+  movies: Movie[];
+  albums: Album[];
+}
+
+
+ function displayItemDetails(item : Book | Movie | Album) {
+   if ("author" in item) {
+     console.log(`Livre : ${item.title} par ${item.author} (${item.year})`);
+   } else if ("director" in item) {
+     console.log(`Film : ${item.title} réalisé par ${item.director} (${item.year})`);
+   } else if ("director" in item) {
+     console.log(`Album : ${item.title} par ${item.artist} (${item.year})`);
+  } else {
+     console.log("Type d'objet inconnu.");
+   }
+ }
+
+
+ data.albums.forEach((album) => {
+  displayItemDetails(album);
+});
+
+data.movies.forEach((movie) => {
+  displayItemDetails(movie);
+});
+
+data.books.forEach((book) => {
+  displayItemDetails(book);
+});
+
+
